146678d27f854c8ea867a472213f7aea2568332e
Jeanet Lin <linett@gmail.com>
2022-03-15 00:08:23 +0100

    iteration 4 - without hex values displayed

--- src/components/BoxColor/BoxColor.js
+++ src/components/BoxColor/BoxColor.js
@@ -1,10 +1,17 @@
 import React from "react";
+import './BoxColor.css';
 
 export default function BoxColor(props) {
-    
+    const divStyle = {
+        backgroundColor: `rgb(${props.r}, ${props.g}, ${props.b})`
+    }
     return (
-        <div>
-        rgb({props.r},)
+        <div className="box_color">
+        <div style={divStyle} className="color">
+        rgb({props.r},{props.g},{props.b})
+        <br></br>
+        #
+        </div>
         </div>
     )
 };
